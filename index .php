3fc8e1110f1f8ef5edbab66d59e24ca1d3c74797
zhangmin <18810652307@163.com>
2016-12-20 10:58:27 +0800

    查看位置7

--- index .php
+++ index .php
@@ -108,8 +108,8 @@ $signPackage = $jssdk->GetSignPackage();
 				        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
 				        var speed = res.speed; // 速度，以米/每秒计
 				        var accuracy = res.accuracy; // 位置精度
-				        alert("经度："+ longitude);
-				        alert("纬度："+ latitude);
+				        /*alert("经度:"+ longitude);
+				        alert("纬度:"+ latitude);*/
 				    }
 				});
 
