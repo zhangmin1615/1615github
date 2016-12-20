feb00a617cf48a6924ccf33786b80bac8a8b6001
zhangmin <18810652307@163.com>
2016-12-20 11:03:21 +0800

    获取当前位置

--- index .php
+++ index .php
@@ -98,27 +98,22 @@ $signPackage = $jssdk->GetSignPackage();
 			     ]
 			  });
 
-			
-
-			function getLocation(){
-				wx.getLocation({
+			 function getLocation(){
+			 	wx.getLocation({
 				    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
 				    success: function (res) {
 				        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
 				        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
 				        var speed = res.speed; // 速度，以米/每秒计
 				        var accuracy = res.accuracy; // 位置精度
-				        /*alert("经度:"+ longitude);
-				        alert("纬度:"+ latitude);*/
 				    }
 				});
+			 }
 
-			}
-
-			var latitude = 0;
-			var longitude = 0;
-			function getPosition(){
-				wx.openLocation({
+			 var latitude=0;
+			 var longitude=0;
+			 function getPosition(){
+			 	wx.openLocation({
 				    latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
 				    longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
 				    name: '', // 位置名
@@ -126,7 +121,8 @@ $signPackage = $jssdk->GetSignPackage();
 				    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
 				    infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
 				});
-			}
+			 }
+
 
 
 		</script>
