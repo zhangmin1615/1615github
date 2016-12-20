ede9c51f5ea542101f01af2bd18ac5de0bdb41ea
zhangmin <18810652307@163.com>
2016-12-20 10:21:27 +0800

    查看位置

--- index .php
+++ index .php
@@ -53,6 +53,7 @@ $signPackage = $jssdk->GetSignPackage();
 		<span class="bottomfoot"></span> -->
 
 		<button onclick="getLocation()">获取地理位置</button>
+		<button onclick="getPosition()">查看位置</button>
 		<script type="text/javascript">
 			 wx.config({
 			    debug: true,
@@ -98,19 +99,32 @@ $signPackage = $jssdk->GetSignPackage();
 			  });
 
 			 function getLocation(){
-			 wx.getLocation({
-			    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
-			    success: function (res) {
-			        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
-			        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
-			        var speed = res.speed; // 速度，以米/每秒计
-			        var accuracy = res.accuracy; // 位置精度
-			        alert("经度："+longitude);
-			        alert("维度："+latitude);
-			    }
-			});
+				 wx.getLocation({
+				    type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+				    success: function (res) {
+				        var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+				        var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+				        var speed = res.speed; // 速度，以米/每秒计
+				        var accuracy = res.accuracy; // 位置精度
+				        alert("经度："+longitude);
+				        alert("维度："+latitude);
+				    }
+				});
 
 			}
+
+			function getPosition(){
+				wx.openLocation({
+				    latitude: latitude, // 纬度，浮点数，范围为90 ~ -90
+				    longitude: longitude, // 经度，浮点数，范围为180 ~ -180。
+				    name: '', // 位置名
+				    address: '', // 地址详情说明
+				    scale: 1, // 地图缩放级别,整形值,范围从1~28。默认为最大
+				    infoUrl: '' // 在查看位置界面底部显示的超链接,可点击跳转
+				});
+			}
+
+
 		</script>
 	</body>
 	
